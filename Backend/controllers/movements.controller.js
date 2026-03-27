f908584c69f6e9254991272c825516bba849c1c8
JHONATANGARCIA <jhonatansgomez.0311@gmail.com>
2026-03-27 15:54:17 -0500

    fix: Correccion en crear movimientos

--- Backend/controllers/movements.controller.js
+++ Backend/controllers/movements.controller.js
@@ -3,12 +3,40 @@ import { db } from "../db.config.js";
 const movementsCtrl = {};
 
 movementsCtrl.saveMovements = async (req, res) => {
+    const client = await db.connect();
     try {
         const { movements } = req.body;
+        await client.query("BEGIN");
 
-        await db.query("BEGIN");
+        const resul = await client.query(`WITH movements_data AS (
+            SELECT * FROM jsonb_to_recordset($1::jsonb) AS m(
+                product_id INT,
+                quantity INT,
+                type TEXT
+            )
+            ),
+            validated_update AS (
+            UPDATE product p
+            SET current_stock = p.current_stock + 
+                (m.quantity * CASE WHEN m.type = 'input' THEN 1 ELSE -1 END)
+            FROM movements_data m
+            WHERE p.id = m.product_id
+                AND (
+                m.type = 'input'
+                OR (m.type = 'output' AND p.current_stock >= m.quantity)
+                )
+            RETURNING p.id
+            )
+            SELECT COUNT(*)::INT FROM validated_update;`,
+            [JSON.stringify(movements)]
+        );
 
-        await db.query(`INSERT INTO movement (product_id, quantity, type) SELECT product_id, quantity, type 
+        if (resul.rows[0].count != movements.length) {
+            await client.query("ROLLBACK");
+            return res.status(400).json({ msg: "No se pudo registrar el movimiento, Verifique el stock de los productos." });
+        }
+
+        await client.query(`INSERT INTO movement (product_id, quantity, type) SELECT product_id, quantity, type 
             FROM jsonb_to_recordset($1::jsonb) 
             AS m(
                 product_id INT, 
@@ -18,23 +46,17 @@ movementsCtrl.saveMovements = async (req, res) => {
             [JSON.stringify(movements)]
         );
 
-        await db.query(`UPDATE product p SET current_stock = current_stock + m.quantity * CASE WHEN m.type = 'input' THEN 1 ELSE -1 END
-            FROM jsonb_to_recordset($1::jsonb) AS m(
-                product_id INT,
-                quantity INT,
-                type TEXT
-            )
-            WHERE p.id = m.product_id`,
-            [JSON.stringify(movements)]
-        );
-
-        await db.query("COMMIT");
+        console.log("FINALIZO EL MOVIMIENTO");
+        await client.query("COMMIT");
         res.status(201).json({ msg: "Movimiento registrado exitosamente." });
     }
     catch (error) {
         await db.query("ROLLBACK");
         console.error(error);
         res.status(500).json({ msg: "Ha ocurrido un error en el servidor, Intenta mas tarde." });
+    }
+    finally {
+        client.release();
     }
 };
 
